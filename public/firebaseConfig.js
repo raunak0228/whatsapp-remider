83115a211e22e838b2069183c41684f234a54dc4
raunak0228 <129169366+raunak0228@users.noreply.github.com>
2025-04-25 15:42:00 +0530

    developer repo init
    developer repo init

---
+++ public/firebaseConfig.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwtNar3TOFPdhimkLkcK7CEdeU1-wEheQ",
+  authDomain: "whatsapp-ab87e.firebaseapp.com",
+  projectId: "whatsapp-ab87e",
+  storageBucket: "whatsapp-ab87e.firebasestorage.app",
+  messagingSenderId: "388788708193",
+  appId: "1:388788708193:web:889edebe3708fe3cdf9b20"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+} 
